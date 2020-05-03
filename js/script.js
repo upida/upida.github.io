0f9d222bb32d2036255f54357aa812e0ad63bc45
Mufida Salma <46271228+upida@users.noreply.github.com>
2020-05-03 15:20:38 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.onscroll = function () {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     $('#road').animate({ top: '0' }, 800);
     document.getElementById('to_my_road').innerHTML = '<a href="/">UPIDA</a>';
   }
