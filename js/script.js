f47c8bb03c601e53e56cef1b8577d39b3cf5c5c5
Mufida Salma <46271228+upida@users.noreply.github.com>
2020-05-04 16:50:48 +0700

    little problem was solve

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 window.onscroll = function () {
-  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-    $('#road').animate({ top: '0' }, 800);
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    $('#road').animate({ top: '0' }, 500);
+    $this = $('#to_my_road');
+    $this.css('text-align', 'left');
+    $this.css('margin', '5px 30px');
     document.getElementById('to_my_road').innerHTML = '<a href="/">UPIDA</a>';
   }
 };
